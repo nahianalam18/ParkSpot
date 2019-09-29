bb4fc4256415bf877f5060ce4dd93483009530be
Nahian Alam <nahian18@outlook.com>
2019-09-28 23:13:13 -0500

    Buttons Disabled

--- lib/infoPage.dart
+++ lib/infoPage.dart
@@ -35,52 +35,52 @@ class _InfoPageState extends State<InfoPage> {
               Spacer(flex: 1),
               RaisedButton(
                 child: Text("View Footage of Lot 26"),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AvailableParking()));
-                },
+                // onPressed: () {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => AvailableParking()));
+                // },
               ),
               Spacer(flex: 1),
               RaisedButton(
                 child: Text("View Footage of Lot 55"),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AvailableParking()));
-                },
+                // onPressed: () {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => AvailableParking()));
+                // },
               ),
               Spacer(flex: 1),
               RaisedButton(
                 child: Text("View Footage of Lot F10"),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AvailableParking()));
-                },
+                // onPressed: () {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => AvailableParking()));
+                // },
               ),
               Spacer(flex: 1),
               RaisedButton(
                 child: Text("View Footage of Lot 36"),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AvailableParking()));
-                },
+                // onPressed: () {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => AvailableParking()));
+                // },
               ),
               Spacer(flex: 1),
               RaisedButton(
                 child: Text("View Footage of Lot F7"),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AvailableParking()));
-                },
+                // onPressed: () {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => AvailableParking()));
+                // },
               ),
               Spacer(flex: 1),
             ],
